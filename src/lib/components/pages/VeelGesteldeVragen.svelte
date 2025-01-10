86c1e21574aaf475d7ac26ab7534695dd309c7f5
Latoya <76912011+latoyaln@users.noreply.github.com>
2025-01-10 11:06:58 +0100

    #28 FAQ geladen

---
+++ src/lib/components/pages/VeelGesteldeVragen.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let items
+</script>
+
+<svelte:head>
+  <title>Veel Gestelde Vragen </title>
+</svelte:head>
+
+<section id="FAQ">
+    {#each items[0].componentsCollection.items as item}
+      <article>
+        <p>{item.textTitle}</p>
+        <p>{item.textParagraph}</p>
+      </article>
+    {/each}
+</section>
