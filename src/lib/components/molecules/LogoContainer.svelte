7adb2d5e06e2abbe03dc543cf95518c60bd903f3
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2025-01-14 15:36:49 +0100

    TW - Copied enhanced images.

--- src/lib/components/molecules/LogoContainer.svelte
+++ src/lib/components/molecules/LogoContainer.svelte
@@ -1,36 +1,26 @@
 <script>
-  // All images
-  import { Vibe, Lillet, Pernod, Bacardi, Hendricks, London, Absolut, CocaCola, Passoa } from '$lib/assets/partners.js' 
   import Logo from "../atoms/Logo.svelte";
 
   // Adjustable aria-attribute
   export let hidden = ''
 </script>
 
-<ul class="logo-container" aria-hidden="{hidden}"> 
-  <Logo source='{Vibe}' alt='Vibe' />
-  <Logo source='{Lillet}' alt='Lillet' />
-  <Logo source='{Pernod}' alt='Pernod Ricard' />
-  <Logo source='{Bacardi}' alt='Bacardi' />
-  <Logo source='{Hendricks}' alt='Hendricks Dry Gin' />
-  <Logo source='{London}' alt='The London Essence' />
-  <Logo source='{Absolut}' alt='Absolut Vodka' />
-  <Logo source='{CocaCola}' alt='Coca Cola' />
-  <Logo source='{Passoa}' alt='Passoa' />
+<ul class="logo-container color-logo" aria-hidden="{hidden}"> 
+  <Logo source='Vibe' alt='Vibe' />
+  <Logo source='Lillet' alt='Lillet' />
+  <Logo source='Pernod' alt='Pernod Ricard' />
+  <Logo source='Bacardi' alt='Bacardi' />
+  <Logo source='Hendricks' alt='Hendricks Dry Gin' />
+  <Logo source='London' alt='The London Essence' />
+  <Logo source='Absolut' alt='Absolut Vodka' />
+  <Logo source='Cocacola' alt='Coca Cola' />
+  <Logo source='Passoa' alt='Passoa' />
 </ul>
 
 <style>
   .logo-container {
     display: inline-block;
     animation: 35s slide infinite linear;
-
-    @media (prefers-reduced-motion) {
-      animation: none;
-      
-      &:last-of-type {
-        display: none;
-      }
-    }
   }
 
   /* Infinite animation for the carrousel */
@@ -42,4 +32,16 @@
       transform: translateX(-100%);
     }
   }
+
+  /* Media Query for users with reduced-motion enabled */
+  /* (Didnt use the 'no-preference' for a reason) */  
+  @media (prefers-reduced-motion) {
+    .logo-container {
+      animation: none;
+    }
+    .logo-container:last-of-type {
+      display: none;
+    }
+  }
 </style>
+
